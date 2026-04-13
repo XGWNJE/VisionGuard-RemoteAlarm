879d3fbc0c24dde2d7c137681656b3048a557704
xgwnje <xgwnje@qq.com>
2026-04-13 22:26:31 +0800

    chore: 版本号更新至 3.0.1

--- receiver/android/app/build.gradle.kts
+++ receiver/android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.xgwnje.visionguard_android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 3001
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

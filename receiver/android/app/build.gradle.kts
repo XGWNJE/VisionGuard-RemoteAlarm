6ed20cc755b3e70f727e13b3e921456566fea651
xgwnje <xgwnje@qq.com>
2026-04-13 22:18:22 +0800

    chore: 初始化三端版本号 3.0.0
    - 新增 VERSION 文件作为版本号唯一来源
    - 同步 server/Android/Windows 三端版本至 3.0.0
    - 建立 prepare-commit-msg hook 实现提交时自动 bump

--- receiver/android/app/build.gradle.kts
+++ receiver/android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.xgwnje.visionguard_android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
